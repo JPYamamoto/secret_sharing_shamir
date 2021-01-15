7276d9701d09842106f4610ef5b277819fe6ec11
Juan Pablo Yamamoto <jpyamamoto@ciencias.unam.mx>
2021-01-15 14:09:18 -0600

    Controller and crypto

--- shamir/controller.py
+++ shamir/controller.py
@@ -15,27 +15,28 @@ class Controller:
         file_content = IO.read_file(entry_file, binary=True)
         aes_result = Crypto.to_aes(file_content, sha_pass)
 
-        coefficients = Controller._sample_random_bits(BITS, t)
-        coefficients.insert(0, sha_pass)
+        coefficients = Controller._sample_random_bits(BITS, t-1)
+        coefficients.insert(0, int.from_bytes(sha_pass, byteorder='little'))
 
         polynomial = Polynomial(coefficients)
         domain = Controller._sample_random_bits(BITS, n)
-        evaluations = [(x, polynomial.evaluate(x)) for x in domain]
+        evaluations = [(x, polynomial.evaluate(x, MODSZ)) for x in domain]
 
         out_frg = '\n'.join(['({},{})'.format(p[0], p[1]) for p in evaluations])
-        IO.write_file(entry_file + '.aes', aes_result)
+        IO.write_file(entry_file + '.aes', aes_result, binary=True)
         IO.write_file(entry_file + '.frg', out_frg)
 
 
     @staticmethod
     def decrypt(points_file, cyphered_file):
-        cyphered_content = IO.read_file(cyphered_file)
+        cyphered_content = IO.read_file(cyphered_file, binary=True)
 
         points_content = IO.read_file(points_file)
         points = Controller._parse_points(points_content)
-        password = Polynomial.lagrange(points, 0)
+        password = Polynomial.lagrange(points, 0, MODSZ)
+        password_bytes = password.to_bytes(BITS // 8, byteorder='little')
 
-        clear_content = Crypto.from_aes(cyphered_content, password)
+        clear_content = Crypto.from_aes(cyphered_content, password_bytes)
         clear_filename = cyphered_file.replace('.aes', '')
         IO.write_file(clear_filename, clear_content, binary=True)
 
@@ -56,7 +57,7 @@ class Controller:
         sample = set()
 
         while len(sample) < k:
-            sample.add(random.getrandbits(bits))
+            sample.add(int(random.getrandbits(bits)))
 
         return list(sample)
 
--- shamir/crypto.py
+++ shamir/crypto.py
@@ -1,13 +1,21 @@
+import hashlib
+from Crypto.Cipher import AES
+
 class Crypto:
     @staticmethod
     def sha_256(text):
-        return text
+        password = hashlib.sha256(text.encode('utf8')).digest()
+        return password
 
     @staticmethod
     def to_aes(text, password):
-        return text
+        aes = AES.new(password, AES.MODE_CFB, 16 * b'\0')
+        encrypted = aes.encrypt(text)
+        return encrypted
 
     @staticmethod
     def from_aes(text, password):
-        return text
+        aes = AES.new(password, AES.MODE_CFB, 16 * b'\0')
+        desencrypted = aes.decrypt(text)
+        return desencrypted
 
